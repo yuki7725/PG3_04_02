024ee2af56c624eac1a96529c838b79fdd40ff15
yuki7725 <147121669+yuki7725@users.noreply.github.com>
2024-06-13 14:03:10 +0900

    プロジェクト ファイルを追加します。

---
+++ main.cpp
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <algorithm>
+#include <vector>
+#include <string.h>
+#include <iostream>
+using namespace std;
+
+template <typename Type1,typename Type2>
+
+class ClassName{
+public:
+	//メンバ変数
+	Type1 a;
+	Type2 b;
+
+	//コンストラクタ
+	ClassName(Type1 a,Type2 b) : a(a),b(b) {}
+
+	//min関数
+	Type min() {
+		if (a < b) {
+			return static_cast<Type1>(a);
+		}
+		else {
+			return static_cast<Type2>(b);
+		}
+	}
+};
+
+int main() {
+	ClassName<int,float> num1(10,20.0f);
+	ClassName<float,double> num2(30.0f,40.0);
+	ClassName<double,int> num3(50.0,60);
+
+	printf("%d\n", (int)num1.min());
+	printf("%f\n", (int)num2.min());
+	printf("%lf\n", (int)num3.min());
+	//coutで出力に書き換える
+	
+	//printf("%d\n", min(num1.a,(int) num2.a));
+
+	/*cout << ("%d\n",num1.min(),num2.min()) << endl;
+	cout << ("%f\n",num2.min(),num3.min()) << endl;
+	cout << ("%lf\n",num1.min(),num3.min()) << endl;*/
+
+	return 0;
+}
